5d570c99cb05b559d482611410eb621c797fa9e9
Sulkhani Otkhozoria <geek838@gmail.com>
2022-12-08 18:36:14 +0400

    add window switching scenario

--- test/specs/home.spec.js
+++ test/specs/home.spec.js
@@ -3,12 +3,15 @@ import Home from "../pages/Home";
 describe('navigate to home page', function () {
     it('should open home page', async function () {
         await browser.url('/')
+        await browser.maximizeWindow()
     });
 });
 
 describe('home page content', function () {
     it('should check if all the pages are displayed', async function () {
-        const expected = await Home.listPages()
+        const pageList = await Home.pages
+        await browser.pause(1000)
+        const expected = pageList.map(page => page.getText())
         expect(expected.length).toEqual(17)
     });
     it('should check names of all pages', async function () {
@@ -23,5 +26,9 @@ describe('perform navigation and window switch', function () {
         await Home.contactUs.click()
         await browser.switchWindow('Contact Us')
         expect(await browser.getTitle()).toEqual('WebDriver | Contact Us')
+    });
+    it('should return to home page',async function () {
+        await browser.switchWindow('https://www.webdriveruniversity.com/')
+        expect(await browser.getTitle()).toEqual('WebDriverUniversity.com')
     });
 });
